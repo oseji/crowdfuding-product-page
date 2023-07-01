f2dc18f67dee03c29c0bc8aec27e52c4eb808588
oseji <jiade1233@gmail.com>
2023-07-01 09:59:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      height: {
+        110: "450px",
+        105: "410px",
+        120: "550px",
+        device: "90vh",
+        85: "345px",
+      },
+      width: {
+        140: "650px",
+        "45/100": "45%",
+        "4/10": "40%",
+      },
+      backgroundImage: {
+        bgDesktop: `url('/images/image-hero-desktop.jpg')`,
+        bgMobile: `url('/images/image-hero-mobile.jpg')`,
+      },
+    },
+  },
+  plugins: [],
+};
