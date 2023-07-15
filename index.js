16f6e72c2591a193072381c4d2662b9ee900ca00
oseji <jiade1233@gmail.com>
2023-07-15 21:48:00 +0100

    commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const modal = document.getElementById("modal");
 const openModal = document.getElementById("openModal");
 const closeModal = document.querySelector(".closeModal");
+const completedModal = document.querySelector(".completedModal");
+const btnComplete = document.getElementById("btnComplete");
 
 const check1 = document.getElementById("check1");
 const check2 = document.getElementById("check2");
@@ -10,13 +12,23 @@ const check3 = document.getElementById("check3");
 const check4 = document.getElementById("check4");
 const allChecks = document.querySelectorAll(".check");
 
+const pledgeBox1 = document.getElementById("box1");
+const pledgeBox2 = document.getElementById("box2");
+const pledgeBox3 = document.getElementById("box3");
+const pledgeBox4 = document.getElementById("box4");
+
 const enterPledge1 = document.getElementById("enterPledge1");
 const enterPledge2 = document.getElementById("enterPledge2");
 const enterPledge3 = document.getElementById("enterPledge3");
 const enterPledge4 = document.getElementById("enterPledge4");
-
-const completedModal = document.querySelector(".completedModal");
-const btnComplete = document.getElementById("btnComplete");
+const pledge25 = document.getElementById("pledge25");
+const pledge75 = document.getElementById("pledge75");
+const pledge200 = document.getElementById("pledge200");
+const pledgeUnlimited = document.getElementById("pledgeUnlimited");
+const pledgeBtn25 = document.getElementById("pledge25btn");
+const pledgeBtn75 = document.getElementById("pledge75btn");
+const pledgeBtn200 = document.getElementById("pledge200btn");
+const pledgeUnlimitedBtn = document.getElementById("pledgeUnlimitedBtn");
 const btnPledge = document.querySelectorAll(".btnPledge");
 
 console.log(
@@ -28,6 +40,7 @@ console.log(
   allChecks
 );
 
+//OPENING PLEDGE MODAL
 openModal.addEventListener("click", () => {
   modal.showModal();
 });
@@ -37,15 +50,18 @@ closeModal.addEventListener("click", () => {
 });
 
 //MAKING A PLEDGE
-
 check1.addEventListener("change", (e) => {
   if (e.target.checked) {
     console.log("just checked");
     enterPledge1.classList.toggle("reveal");
     enterPledge1.classList.toggle("close");
+    pledgeBox1.classList.toggle("pledgeBorderInactive");
+    pledgeBox1.classList.toggle("pledgeBorderActive");
   } else {
     enterPledge1.classList.toggle("reveal");
     enterPledge1.classList.toggle("close");
+    pledgeBox1.classList.toggle("pledgeBorderInactive");
+    pledgeBox1.classList.toggle("pledgeBorderActive");
   }
 });
 
@@ -54,9 +70,13 @@ check2.addEventListener("change", (e) => {
     console.log("just checked");
     enterPledge2.classList.toggle("reveal");
     enterPledge2.classList.toggle("close");
+    pledgeBox2.classList.toggle("pledgeBorderInactive");
+    pledgeBox2.classList.toggle("pledgeBorderActive");
   } else {
     enterPledge2.classList.toggle("reveal");
     enterPledge2.classList.toggle("close");
+    pledgeBox2.classList.toggle("pledgeBorderInactive");
+    pledgeBox2.classList.toggle("pledgeBorderActive");
   }
 });
 
@@ -65,9 +85,13 @@ check3.addEventListener("change", (e) => {
     console.log("just checked");
     enterPledge3.classList.toggle("reveal");
     enterPledge3.classList.toggle("close");
+    pledgeBox3.classList.toggle("pledgeBorderInactive");
+    pledgeBox3.classList.toggle("pledgeBorderActive");
   } else {
     enterPledge3.classList.toggle("reveal");
     enterPledge3.classList.toggle("close");
+    pledgeBox3.classList.toggle("pledgeBorderInactive");
+    pledgeBox3.classList.toggle("pledgeBorderActive");
   }
 });
 
@@ -76,60 +100,63 @@ check4.addEventListener("change", (e) => {
     console.log("just checked");
     enterPledge4.classList.toggle("reveal");
     enterPledge4.classList.toggle("close");
+    pledgeBox4.classList.toggle("pledgeBorderInactive");
+    pledgeBox4.classList.toggle("pledgeBorderActive");
   } else {
     enterPledge4.classList.toggle("reveal");
     enterPledge4.classList.toggle("close");
+    pledgeBox4.classList.toggle("pledgeBorderInactive");
+    pledgeBox4.classList.toggle("pledgeBorderActive");
   }
 });
 
 //SUBMITTING A PLEDGE TO REVEAL COMPLETED MODAL
+// btnPledge.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     completedModal.showModal();
+//   });
+// });
+
 btnPledge.forEach((btn) => {
   btn.addEventListener("click", () => {
-    completedModal.showModal();
+    if (btn.id == "pledge25btn") {
+      if (pledge25.value >= 25) {
+        completedModal.showModal();
+        pledge25.value = "";
+      } else {
+        alert("Must donate at least $25");
+      }
+    }
+
+    if (btn.id == "pledge75btn") {
+      if (pledge75.value >= 75) {
+        completedModal.showModal();
+        pledge75.value = "";
+      } else {
+        alert("Must donate at least $75");
+      }
+    }
+
+    if (btn.id == "pledge200btn") {
+      if (pledge200.value >= 200) {
+        completedModal.showModal();
+        pledge200.value = "";
+      } else {
+        alert("Must donate at least $200");
+      }
+    }
+
+    if (btn.id == "pledgeUnlimitedBtn") {
+      if (pledgeUnlimited.value > 0) {
+        completedModal.showModal();
+        pledgeUnlimited.value = "";
+      } else {
+        alert("Must be greater than $0");
+      }
+    }
   });
 });
 
 btnComplete.addEventListener("click", () => {
   completedModal.close();
 });
-
-// allChecks.forEach((check) => {
-//   //console.log(check);
-//   check.addEventListener("change", (e) => {
-//     if ((e.target.id = "check1")) {
-//       console.log("just checked 1");
-//       enterPledge1.classList.toggle("reveal");
-//       enterPledge1.classList.toggle("close");
-//     } else {
-//       enterPledge1.classList.toggle("reveal");
-//       enterPledge1.classList.toggle("close");
-//     }
-
-//     if ((e.target.id = "check2")) {
-//       console.log("just checked 2");
-//       enterPledge2.classList.toggle("reveal");
-//       enterPledge2.classList.toggle("close");
-//     } else {
-//       enterPledge2.classList.toggle("reveal");
-//       enterPledge2.classList.toggle("close");
-//     }
-
-//     if ((e.target.id = "check3")) {
-//       console.log("just checked 3");
-//       enterPledge3.classList.toggle("reveal");
-//       enterPledge3.classList.toggle("close");
-//     } else {
-//       enterPledge3.classList.toggle("reveal");
-//       enterPledge3.classList.toggle("close");
-//     }
-
-//     if ((e.target.id = "check4")) {
-//       console.log("just checked 4");
-//       enterPledge4.classList.toggle("reveal");
-//       enterPledge4.classList.toggle("close");
-//     } else {
-//       enterPledge4.classList.toggle("reveal");
-//       enterPledge4.classList.toggle("close");
-//     }
-//   });
-// });
